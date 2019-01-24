4c650b3e30a82e287b908cc565154fc3d96683d2
sergiorock <lopezer.nic@gmail.com>
2019-01-24 07:02:09 -0300

    add articles resources and style navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  
-  get 'welcome/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
+  
+=begin  
+    get "/articles"
+    post "/articles"
+    delete "/articles"
+    get "/articles/:id"
+    get "/articles/new"
+    get "/articles/:id/edit"
+    patch "/articles/:id"
+    put "/articles/:id"
+=end
   
 end
