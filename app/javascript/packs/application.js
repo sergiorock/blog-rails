2d110b3f75e55d189703430d11ba7e83f7afcca8
Sergio Lopez <lopez.sergio.n@gmail.com>
2022-02-18 16:00:19 -0300

    add bootstrap 5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,12 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// Importa la parte JS de bootstrap
+import 'bootstrap'
+
+// Importal el CSS de bootstrap
+import 'css/styles'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Get es el método que se usa para obtener información
+  # /welcome es la ruta que se usa para acceder a esa vista
+  # to: indica el controlador y la acción que se va a ejecutar
+  # home es el controller
+  # index es el action
+
+  get "/welcome", to: "home#index"
 end
