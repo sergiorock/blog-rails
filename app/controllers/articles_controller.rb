5be1c0f99d3e97b5729e8ab586d3de1cee113308
Sergio Lopez <lopez.sergio.n@gmail.com>
2022-02-18 20:04:59 -0300

    allow show articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     # En el caso particular de rails una variable de instancia (@) se puede llamar desde la vista que esta llamando a esta acción
     # Mientras que si declaramos la variable sin el @ se puede usar solo en el método
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   get "/welcome", to: "home#index"
 
   get "articles/new", to: "articles#new"
+  get "articles/:id", to: "articles#show"
   post "articles", to: "articles#create"
 end
