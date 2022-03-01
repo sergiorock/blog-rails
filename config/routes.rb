de7ba0dcb47b595b191cd247df102e34ef960661
Sergio Lopez <lopez.sergio.n@gmail.com>
2022-03-01 17:48:10 -0300

    allow upload profile picture

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
@@ -17,6 +18,9 @@ Rails.application.routes.draw do
   end
 
   resources :categories
+
+  resources :users, only: [:update]
+  get "profile", to: "users#edit"
 
   # get "articles/user/:user_id", to: "articles#from_author"
 
